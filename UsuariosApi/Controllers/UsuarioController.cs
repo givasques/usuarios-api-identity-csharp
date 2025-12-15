472e1dc0913eb247c98b54fe579b31250807e0cc
unknown <giovanna.vasques1101@hotmail.com>
2025-12-15 16:08:48 -0300

    subindo o projeto com conexão ao banco de dados com EF Core e implementação do Identity para cadastro de usuários com a lógica na camada Service isolando a Controller

---
+++ UsuariosApi/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UsuariosApi.Data;
+using UsuariosApi.Data.Dtos;
+using UsuariosApi.Models;
+using UsuariosApi.Profiles;
+using UsuariosApi.Services;
+
+namespace UsuariosApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+    private CadastroService _cadastroService;
+
+    public UsuarioController(CadastroService cadastroService)
+    {
+        _cadastroService = cadastroService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CadastraUsuario([FromBody] CreateUsuarioDto dto)
+    {
+        await _cadastroService.Cadastra(dto);
+        return Ok("Usuário cadastrado!");
+    }
+}
